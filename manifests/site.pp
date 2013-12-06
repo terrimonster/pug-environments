531386c2222d73eebfa9953a631091133e75531a
Terri Haber <terrimonster@gmail.com>
2013-12-05 20:23:06 -0800

    production

---
+++ manifests/site.pp
@@ -0,0 +1,25 @@
+# Define filebucket 'main':
+filebucket { 'main':
+  server => 'master.puppetlabs.vm',
+  path   => false,
+}
+
+# Make filebucket 'main' the default backup location for all File resources:
+File { backup => 'main' }
+
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
+
+# The default node definition matches any node lacking a more specific node
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
+
+node default {
+  # This is where you can declare classes for all nodes.
+  # Example:
+  #   class { 'my_class': }
+  hiera_include('classes')
+}
